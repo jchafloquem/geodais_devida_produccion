63e284178ab628925ac028e325811caa44a4a0ca
Jorge Chafloque Millones <37223352+jchafloquem@users.noreply.github.com>
2025-11-14 09:12:24 -0500

    Mostrar el manual

--- src/app/geovisor/components/sidebar/components/manual/manual.component.ts
+++ src/app/geovisor/components/sidebar/components/manual/manual.component.ts
@@ -1,38 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { ManualDialogComponent } from './manual-dialog/manual-dialog.component';
 
 @Component({
   selector: 'app-manual',
   standalone: true,
-  imports: [CommonModule, MatDialogModule, MatButtonModule, MatIconModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule],
   templateUrl: './manual.component.html',
   styleUrl: './manual.component.scss'
 })
-export class ManualComponent implements OnInit {
+export class ManualComponent {
 
   manualUrl = 'assets/manual/ManualGeoVisor.pdf';
-  safeManualUrl: SafeResourceUrl | null = null;
 
-  constructor(private sanitizer: DomSanitizer, private dialog: MatDialog) { }
-
-  ngOnInit(): void {
-    this.safeManualUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.manualUrl);
-  }
-
-  openManualModal(): void {
-    this.dialog.open(ManualDialogComponent, {
-      width: '90vw',
-      height: '90vh',
-      maxWidth: '1200px',
-      data: {
-        safeUrl: this.safeManualUrl,
-        rawUrl: this.manualUrl
-      }
-    });
-  }
+  constructor() { }
 }
